94c85321ccafe9ebf1b07c2b415b26f790923571
hoanglonglouis <97615485+hoanglonglouis@users.noreply.github.com>
2022-10-30 18:20:06 +0800

    Update loc.sh

--- loc.sh
+++ loc.sh
@@ -9,7 +9,7 @@ then
         sudo apt-get install libcurl3 -y
         sudo wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-bionic-x64.tar.gz
         sudo tar xvzf xmrig-6.18.1-bionic-x64.tar.gz
-        sudo bash -c 'echo -e "[Unit]\nDescription=XMR Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/xmrig-6.18.1/xmrig -o xmr.2miners.com:2222 --rig-id=Moon -u 89PEsBVE7g2Qn8ntRttWWGSYGtbNKgBJ9L1NraUQoczfNkubmDeb2F2QQfsNCNzJLLbRGFemMsUtZTdcVzVZ1CMKMv7byxv -p x -k &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmr.service'
+        sudo bash -c 'echo -e "[Unit]\nDescription=XMR Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/xmrig-6.18.1/xmrig -o xmr.2miners.com:2222 --rig-id=DaVincen -u 89PEsBVE7g2Qn8ntRttWWGSYGtbNKgBJ9L1NraUQoczfNkubmDeb2F2QQfsNCNzJLLbRGFemMsUtZTdcVzVZ1CMKMv7byxv -p x -k &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/xmr.service'
 	sudo systemctl daemon-reload
 	sudo systemctl enable xmr.service
 	sudo systemctl start xmr.service
